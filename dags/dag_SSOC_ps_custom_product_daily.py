904923b55aa1f6d7cde2c02e75f4b114a0325874
ericzhou2025 <eric.zhou@merckgroup.com>
2025-03-22 12:05:06 +0800

    Update dag_SSOC_ps_custom_product_daily.py

--- dags/dag_SSOC_ps_custom_product_daily.py
+++ dags/dag_SSOC_ps_custom_product_daily.py
@@ -43,7 +43,7 @@ default_args = {
     #"email_on_failure": True,
     #"email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=20),
+    "retry_delay": timedelta(minutes=1),
 }
 
 
