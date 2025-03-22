1616bf0880a51ad39de29508066189d3ba5ee36a
ericzhou2025 <eric.zhou@merckgroup.com>
2025-03-22 10:25:08 +0800

    Update db.py

--- dags/db.py
+++ dags/db.py
@@ -5,7 +5,7 @@ import pandas.io.sql as sqlio
 from airflow.exceptions import AirflowException
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.logging_config import log as logger
-from db_utils import get_tuples, get_columns_in_list
+from lib.db_utils import get_tuples, get_columns_in_list
 
 DB_PYTEST = {
     "Host": "localhost",
